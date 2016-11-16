35b565ac10057b4a4c6da2e10110f3c14a3710c1
george <tohearstories@gmail.com>
2016-11-15 19:23:50 -0500

    finished

--- app/src/main/java/edu/temple/mobilelabseven/MainActivity.java
+++ app/src/main/java/edu/temple/mobilelabseven/MainActivity.java
@@ -1,36 +1,111 @@
 package edu.temple.mobilelabseven;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    int i=0;            //i is the total number of open "tabs"
+    int currentIndex=0; //currentIndex is the "tab" currently displayed
+    ArrayList<Fragment> openTabs = new ArrayList();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        openTabs.add(new WebPageFragment());//loads the webview initially
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragmentTransaction.add(R.id.actionBarLayout, new ActionBarFragment());
-        fragmentTransaction.commit();
+        fragmentManager = getFragmentManager();
 
         fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.addressBarLayout, new AddressBarFragment());
+        fragmentTransaction.add(R.id.webPageLayout, openTabs.get(i));//loads the webview initially
+
+        Uri data = getIntent().getData();
+        if(data != null) {
+            String url = data.toString();
+            (  (WebPageFragment) openTabs.get(i)).setUrlToLoad(data.toString());
+        }
+
         fragmentTransaction.commit();
 
+    }
+
+
+
+    public void newButtonClicked() {
+        openTabs.add(new WebPageFragment());
+        i++;
+        currentIndex = i;
         fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.webPageLayout, new WebPageFragment());
+        fragmentTransaction.replace(R.id.webPageLayout, openTabs.get(i));
         fragmentTransaction.commit();
+    }
 
+    public void backButtonClicked() {
 
+        if (currentIndex-1 < 0) {
+            Toast.makeText(getApplicationContext(), "can not go back any further", Toast.LENGTH_SHORT).show();
+        }
 
+        if ( currentIndex-1  >  0 || currentIndex-1 == 0) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.webPageLayout, openTabs.get(currentIndex - 1));
+            fragmentTransaction.commit();
+            currentIndex--;
+        }
+    }
 
+    public void forwardButtonClicked() {
 
+        if (currentIndex +1 > i){
+            Toast.makeText(getApplicationContext(), "can not go forward any further", Toast.LENGTH_SHORT).show();
+        }
 
+        if ( currentIndex+1  <  i || currentIndex+1 == i) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.webPageLayout, openTabs.get(currentIndex + 1));
+            fragmentTransaction.commit();
+            currentIndex++;
+        }
+
+    }
+
+//what follows is necessary overhead to create the back, next, and new buttons/links on the appbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //this basically sets the onClick behavior for the back, next, and new buttons/links on the appbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.backButton){
+            backButtonClicked();
+        }
+
+        if (item.getItemId() == R.id.forwardButton){
+            forwardButtonClicked();
+        }
+
+        if (item.getItemId() == R.id.newButton){
+            newButtonClicked();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/edu/temple/mobilelabseven/WebPageFragment.java
+++ app/src/main/java/edu/temple/mobilelabseven/WebPageFragment.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class WebPageFragment extends Fragment {
+
+    WebView webView;
+    EditText url;
+
+    String urlToLoad;
 
     public static WebPageFragment newInstance() {
         return new WebPageFragment();
@@ -28,14 +36,39 @@ public class WebPageFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.activity_web_page_fragment, container, false);
 
-        WebView webView = (WebView) v.findViewById(R.id.webView);
-        //webView.loadUrl("http://www.google.com");
+        webView = (WebView) v.findViewById(R.id.webView);
+
+        if (urlToLoad != null) {
+            webView.loadUrl(urlToLoad);
+        }
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        url = (EditText)  v.findViewById(R.id.urlText);
+        Button goButton = (Button) v.findViewById(R.id.goButton);
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                urlToLoad = url.getText().toString();
+
+                if( ! urlToLoad.startsWith("http://")  ){
+                    urlToLoad = "http://" + urlToLoad;
+                }
+
+                webView.loadUrl(  urlToLoad );
+            }
+        });
 
         return v;
     }
 
+    public String getUrlToLoad() {
+        return urlToLoad;
+    }
 
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(int position);
+    public void setUrlToLoad(String urlToLoad) {
+        this.urlToLoad = urlToLoad;
     }
 }
